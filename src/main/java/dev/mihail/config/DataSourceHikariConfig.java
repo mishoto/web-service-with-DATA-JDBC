ad27ce20b1d2b84825e9f9f44d6bdfb7d11b7dd8
Mishoto <mihail.dev.io@gmail.com>
2021-10-26 16:16:10 +0300

    Hikari datasource config created

---
+++ src/main/java/dev/mihail/config/DataSourceHikariConfig.java
@@ -0,0 +1,28 @@
+package dev.mihail.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DataSourceHikariConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.main")
+    public HikariDataSource hikariDataSource(){
+       return DataSourceBuilder.create()
+               .type(HikariDataSource.class)
+               .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplateOfHikari(HikariDataSource hikariDataSource){
+        return new JdbcTemplate(hikariDataSource);
+    }
+
+}

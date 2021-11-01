5a2b9f6d13b083634b526e06ad63e441c6eb791f
Mishoto <mihail.dev.io@gmail.com>
2021-11-01 08:13:20 +0200

    create UserRepositoryTest

---
+++ src/test/java/dev/mihail/testUserDAO/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package dev.mihail.testUserDAO;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJdbcTest
+public class UserRepositoryTest {
+
+
+}
